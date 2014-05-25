6b77ced614e7a9378df11ee91863af2e445b1ac6
Jean-Claude Moissinac <jcmoissinac@gmail.com>
2014-05-25 19:42:17 +0200

    add q, Q, c, C commands

--- SuperPathExpander.js
+++ SuperPathExpander.js
@@ -103,6 +103,22 @@ var superpath = function()
            var targetPt = cmd.parameters[1];
            newdata += "Q"+ctrlPt.x+","+ctrlPt.y+" "+pt.x+","+pt.y;
         break;
+        case 'c':
+           var ctrlPt1 = cmd.parameters[0];
+           var ctrlPt2 = cmd.parameters[1];
+           var targetPt = cmd.parameters[2];
+           newdata += "c"+
+                (-targetPt.x+ctrlPt2.x)+","+(-targetPt.y+ctrlPt2.y)+" "+
+                (-targetPt.x+ctrlPt1.x)+","+(-targetPt.y+ctrlPt1.y)+" "+
+                (-1.0*targetPt.x)+","+(-1.0*targetPt.y);
+        break;
+        case 'C':
+          var pt = cmdList[i-1].endPt;
+           var ctrlPt1 = cmd.parameters[0];
+           var ctrlPt2 = cmd.parameters[1];
+           var targetPt = cmd.parameters[2];
+           newdata += "C"+ctrlPt2.x+","+ctrlPt2.y+" "+ctrlPt1.x+","+ctrlPt1.y+" "+pt.x+","+pt.y;
+        break;
         case '':
           crtPt = cmd.current; 
           newdata += "l0,0";//"L"+crtPt.x+","+crtPt.y;
@@ -116,295 +132,295 @@ var superpath = function()
   
   function svg_parse_iri(attribute_content)
   {
-  var iri = new Object();
-  // alert(attribute_content);
-  if (attribute_content[0] == '#') {
-  iri.string = attribute_content;
-  iri.target = document.getElementById(attribute_content.slice(1));
-  // alert(iri.target);
-  if (!iri.target) {
-  iri.type = superpath.XMLRI_STRING;
-  } else {
-  iri.type = superpath.XMLRI_ELEMENTID;
-  }
-  } else {
-  iri.type = superpath.XMLRI_STRING;
-  iri.string = attribute_content;
-  }
-  return iri;
+    var iri = new Object();
+    // alert(attribute_content);
+    if (attribute_content[0] == '#') {
+    iri.string = attribute_content;
+    iri.target = document.getElementById(attribute_content.slice(1));
+    // alert(iri.target);
+    if (!iri.target) {
+    iri.type = superpath.XMLRI_STRING;
+    } else {
+    iri.type = superpath.XMLRI_ELEMENTID;
+    }
+    } else {
+    iri.type = superpath.XMLRI_STRING;
+    iri.string = attribute_content;
+    }
+    return iri;
   }
   
   function svg_parse_idref(attribute_content)
   {
-  var iri = new Object();
-  iri.type = superpath.XMLRI_ELEMENTID;
-  iri.target = document.getElementById(attribute_content);
-  if (!iri.target) {
-  iri.string = gf_strdup(attribute_content);
-  }
+    var iri = new Object();
+    iri.type = superpath.XMLRI_ELEMENTID;
+    iri.target = document.getElementById(attribute_content);
+    if (!iri.target) {
+      iri.string = gf_strdup(attribute_content);
+    }
   }
   
   // parsing path ; source inspired from canvg library
   function svg_parse_path(attribute_content) 
   {
-  var d = attribute_content;
-  this.compressSpaces = function(s) { return s.replace(/[\s\r\t\n]+/gm,' '); }
-  this.trim = function(s) { return s.replace(/^\s+|\s+$/g, ''); }
-  
-  // TODO: convert to real lexer based on http://www.w3.org/TR/SVG11/paths.html#PathDataBNF
-  d = d.replace(/,/gm,' '); // get rid of all commas
-  d = d.replace(/([MmZzLlHhVvCcSsQqTtAa])([MmZzLlHhVvCcSsQqTtAa])/gm,'$1 $2'); // separate commands from commands
-  d = d.replace(/([MmZzLlHhVvCcSsQqTtAa])([MmZzLlHhVvCcSsQqTtAa])/gm,'$1 $2'); // separate commands from commands
-  d = d.replace(/([MmZzLlHhVvCcSsQqTtAa])([^\s])/gm,'$1 $2'); // separate commands from points
-  d = d.replace(/([^\s])([MmZzLlHhVvCcSsQqTtAa])/gm,'$1 $2'); // separate commands from points
-  d = d.replace(/([0-9])([+\-])/gm,'$1 $2'); // separate digits when no comma
-  d = d.replace(/(\.[0-9]*)(\.)/gm,'$1 $2'); // separate digits when no comma
-  d = d.replace(/([Aa](\s+[0-9]+){3})\s+([01])\s*([01])/gm,'$1 $3 $4 '); // shorthand elliptical arc path syntax
-  d = this.compressSpaces(d); // compress multiple spaces
-  d = this.trim(d);
-  this.PathParser = new (function(d) {
-  	this.tokens = d.split(' ');
-  	
-  	this.reset = function() {
-  		this.i = -1;
-  		this.command = '';
-  		this.previousCommand = '';
-  		this.start = new this.Point(0, 0);
-  		this.control = new this.Point(0, 0);
-  		this.current = new this.Point(0, 0);
-  		this.points = [];
-  		this.angles = [];
-  	}
-  					
-  	this.isEnd = function() {
-  		return this.i >= this.tokens.length - 1;
-  	}
-  	
-  	this.isCommandOrEnd = function() {
-  		if (this.isEnd()) return true;
-  		return this.tokens[this.i + 1].match(/^[A-Za-z]$/) != null;
-  	}
-  	
-  	this.isRelativeCommand = function() {
-  		switch(this.command)
-  		{
-  			case 'm':
-  			case 'l':
-  			case 'h':
-  			case 'v':
-  			case 'c':
-  			case 's':
-  			case 'q':
-  			case 't':
-  			case 'a':
-  			case 'z':
-  				return true;
-  				break;
-  		}
-  		return false;
-  	}
-  				
-  	this.getToken = function() {
-  		this.i++;
-  		return this.tokens[this.i];
-  	}
-  	
-  	this.getScalar = function() {
-  		return parseFloat(this.getToken());
-  	}
-  	
-  	this.nextCommand = function() {
-  		this.previousCommand = this.command;
-  		this.command = this.getToken();
-  	}				
-  	
-  	this.getPoint = function(relative) { // if relative is false, make the point absolute
-  		var p = new this.Point(this.getScalar(), this.getScalar());
-  		return (relative?p:this.makeAbsolute(p));
-  	}
-  	
-  	this.getAsControlPoint = function(relative) {
-  		var p = this.getPoint(relative);
-  		this.control = p;
-  		return p;
-  	}
-  	
-  	this.getAsCurrentPoint = function(relative) {
-  		var p = this.getPoint(relative);
-  		this.current = p;
-  		return p;	
-  	}
-  	
-  	this.getReflectedControlPoint = function() {
-  		if (this.previousCommand.toLowerCase() != 'c' && 
-  		    this.previousCommand.toLowerCase() != 's' &&
-  			this.previousCommand.toLowerCase() != 'q' && 
-  			this.previousCommand.toLowerCase() != 't' ){
-  			return this.current;
-  		}
-  		
-  		// reflect point
-  		var p = new this.Point(2 * this.current.x - this.control.x, 2 * this.current.y - this.control.y);					
-  		return p;
-  	}
-  	
-    this.Point = function(x, y) {
-  		this.x = x;
-  		this.y = y;
-  	}	
-  
-  	this.makeAbsolute = function(p) {
-  		if (this.isRelativeCommand()) {
-  			p.x += this.current.x;
-  			p.y += this.current.y;
-  		}
-  		return p;
-  	}
-  	
-  })(d);
-  
-  // get an command list representation of the path
-  {
-  	var pp = this.PathParser;
-  	var cmdList = new Array();
-  	cmdList.toString= function(){
-  	  var i = 0;
-  	  var str = "";
-  	  while (i < this.length)
-  	  {
-  	      str += this[i].command + " " + this[i].parameters + "\n";
-  	      i++;
+    var d = attribute_content;
+    this.compressSpaces = function(s) { return s.replace(/[\s\r\t\n]+/gm,' '); }
+    this.trim = function(s) { return s.replace(/^\s+|\s+$/g, ''); }
+    
+    // TODO: convert to real lexer based on http://www.w3.org/TR/SVG11/paths.html#PathDataBNF
+    d = d.replace(/,/gm,' '); // get rid of all commas
+    d = d.replace(/([MmZzLlHhVvCcSsQqTtAa])([MmZzLlHhVvCcSsQqTtAa])/gm,'$1 $2'); // separate commands from commands
+    d = d.replace(/([MmZzLlHhVvCcSsQqTtAa])([MmZzLlHhVvCcSsQqTtAa])/gm,'$1 $2'); // separate commands from commands
+    d = d.replace(/([MmZzLlHhVvCcSsQqTtAa])([^\s])/gm,'$1 $2'); // separate commands from points
+    d = d.replace(/([^\s])([MmZzLlHhVvCcSsQqTtAa])/gm,'$1 $2'); // separate commands from points
+    d = d.replace(/([0-9])([+\-])/gm,'$1 $2'); // separate digits when no comma
+    d = d.replace(/(\.[0-9]*)(\.)/gm,'$1 $2'); // separate digits when no comma
+    d = d.replace(/([Aa](\s+[0-9]+){3})\s+([01])\s*([01])/gm,'$1 $3 $4 '); // shorthand elliptical arc path syntax
+    d = this.compressSpaces(d); // compress multiple spaces
+    d = this.trim(d);
+    this.PathParser = new (function(d) {
+    	this.tokens = d.split(' ');
+    	
+    	this.reset = function() {
+    		this.i = -1;
+    		this.command = '';
+    		this.previousCommand = '';
+    		this.start = new this.Point(0, 0);
+    		this.control = new this.Point(0, 0);
+    		this.current = new this.Point(0, 0);
+    		this.points = [];
+    		this.angles = [];
+    	}
+    					
+    	this.isEnd = function() {
+    		return this.i >= this.tokens.length - 1;
+    	}
+    	
+    	this.isCommandOrEnd = function() {
+    		if (this.isEnd()) return true;
+    		return this.tokens[this.i + 1].match(/^[A-Za-z]$/) != null;
+    	}
+    	
+    	this.isRelativeCommand = function() {
+    		switch(this.command)
+    		{
+    			case 'm':
+    			case 'l':
+    			case 'h':
+    			case 'v':
+    			case 'c':
+    			case 's':
+    			case 'q':
+    			case 't':
+    			case 'a':
+    			case 'z':
+    				return true;
+    				break;
+    		}
+    		return false;
+    	}
+    				
+    	this.getToken = function() {
+    		this.i++;
+    		return this.tokens[this.i];
+    	}
+    	
+    	this.getScalar = function() {
+    		return parseFloat(this.getToken());
+    	}
+    	
+    	this.nextCommand = function() {
+    		this.previousCommand = this.command;
+    		this.command = this.getToken();
+    	}				
+    	
+    	this.getPoint = function(relative) { // if relative is false, make the point absolute
+    		var p = new this.Point(this.getScalar(), this.getScalar());
+    		return (relative?p:this.makeAbsolute(p));
+    	}
+    	
+    	this.getAsControlPoint = function(relative) {
+    		var p = this.getPoint(relative);
+    		this.control = p;
+    		return p;
+    	}
+    	
+    	this.getAsCurrentPoint = function(relative) {
+    		var p = this.getPoint(relative);
+    		this.current = p;
+    		return p;	
+    	}
+    	
+    	this.getReflectedControlPoint = function() {
+    		if (this.previousCommand.toLowerCase() != 'c' && 
+    		    this.previousCommand.toLowerCase() != 's' &&
+    			this.previousCommand.toLowerCase() != 'q' && 
+    			this.previousCommand.toLowerCase() != 't' ){
+    			return this.current;
+    		}
+    		
+    		// reflect point
+    		var p = new this.Point(2 * this.current.x - this.control.x, 2 * this.current.y - this.control.y);					
+    		return p;
+    	}
+    	
+      this.Point = function(x, y) {
+    		this.x = x;
+    		this.y = y;
+    	}	
+    
+    	this.makeAbsolute = function(p) {
+    		if (this.isRelativeCommand()) {
+    			p.x += this.current.x;
+    			p.y += this.current.y;
+    		}
+    		return p;
+    	}
+    	
+    })(d);
+    
+    // get an command list representation of the path
+    {
+    	var pp = this.PathParser;
+    	var cmdList = new Array();
+    	cmdList.toString= function(){
+    	  var i = 0;
+    	  var str = "";
+    	  while (i < this.length)
+    	  {
+    	      str += this[i].command + " " + this[i].parameters + "\n";
+    	      i++;
+        }
+        return str;
       }
-      return str;
+    	pp.reset();
+    
+    	while (!pp.isEnd()) {
+    		pp.nextCommand();
+    		switch (pp.command) {
+    		case 'M':
+    		case 'm':
+    			var p = pp.getAsCurrentPoint(pp.isRelativeCommand());
+    			var cmd = new Object();
+          cmd.command = pp.command;
+          cmd.parameters = new Array();
+          cmd.parameters.push(p);
+          cmdList.push(cmd);
+    			pp.start = pp.current;
+    			while (!pp.isCommandOrEnd()) {
+    				var p = pp.getAsCurrentPoint(pp.isRelativeCommand());
+    				var cmd = new Object();
+            cmd.command = "M";
+            cmd.parameters = new Array();
+            cmd.parameters.push(p);
+            cmdList.push(cmd);
+    			}
+    			break;
+    		case 'L':
+    		case 'l':
+    			while (!pp.isCommandOrEnd()) {
+    				var cmd = new Object();
+            cmd.current = pp.current;
+    				var p = pp.getAsCurrentPoint(pp.isRelativeCommand());
+            cmd.command = pp.command;
+            cmd.parameters = new Array();
+            cmd.parameters.push(p);
+            cmd.endPt = p;
+            cmdList.push(cmd);
+    			}
+    			break;
+    		case 'H':
+    		case 'h':
+    			while (!pp.isCommandOrEnd()) {
+    				var cmd = new Object();
+            cmd.current = pp.current;
+    			  var coord = pp.getScalar();
+    			  var t =   pp.isRelativeCommand();
+    				var newP = new pp.Point((pp.isRelativeCommand() ? pp.current.x : 0) + coord, pp.current.y);
+    				pp.current = newP;
+            cmd.command = pp.command;
+            cmd.parameters = new Array();
+            cmd.parameters.push((pp.isRelativeCommand() ? pp.current.x : 0) + coord);
+            cmd.endPt = newP;
+            cmdList.push(cmd);
+    			}
+    			break;
+    		case 'V':
+    		case 'v':
+    			while (!pp.isCommandOrEnd()) {
+    				var cmd = new Object();
+            cmd.current = pp.current;
+    			  var coord = pp.getScalar();
+    				var newP = new pp.Point(pp.current.x, (pp.isRelativeCommand() ? pp.current.y : 0) + coord);
+    				pp.current = newP;
+            cmd.command = pp.command;
+            cmd.parameters = new Array();
+            cmd.parameters.push((pp.isRelativeCommand() ? pp.current.x : 0) + coord);
+            cmd.endPt = newP;
+            cmdList.push(cmd);
+    			}
+    			break;
+    		case 'C':
+    		case 'c':
+          while (!pp.isCommandOrEnd()) {
+    				var cmd = new Object();
+            cmd.current = pp.current;
+    				var cntrl1 = pp.getAsControlPoint(pp.isRelativeCommand());
+    				var cntrl2 = pp.getAsControlPoint(pp.isRelativeCommand());
+    				var cp = pp.getAsCurrentPoint(pp.isRelativeCommand());
+            cmd.command = pp.command;
+            cmd.parameters = new Array();
+            cmd.parameters.push(cntrl1);
+            cmd.parameters.push(cntrl2);
+            cmd.parameters.push(cp);
+            cmdList.push(cmd);
+    			}
+          break;
+    		case 'S':
+    		case 's':
+    			/*
+          while (!pp.isCommandOrEnd()) {
+    				var curr = pp.current;
+    				var p1 = pp.getReflectedControlPoint();
+    				var cntrl = pp.getAsControlPoint(pp.isRelativeCommand());
+    				var cp = pp.getAsCurrentPoint(pp.isRelativeCommand());
+    				pp.addMarker(cp, cntrl, p1);
+    				bb.addBezierCurve(curr.x, curr.y, p1.x, p1.y, cntrl.x, cntrl.y, cp.x, cp.y);
+    			}
+    			*/
+          break;
+    		case 'Q':
+    		case 'q':
+          while (!pp.isCommandOrEnd()) {
+    				var cmd = new Object();
+            cmd.current = pp.current;
+    				var cntrl = pp.getAsControlPoint(pp.isRelativeCommand());
+    				var cp = pp.getAsCurrentPoint(pp.isRelativeCommand());
+            cmd.command = pp.command;
+            cmd.parameters = new Array();
+            cmd.parameters.push(cntrl);
+            cmd.parameters.push(cp);
+            cmd.endPt = cp;
+            cmdList.push(cmd);
+    			}
+          break;
+    		case 'T':
+    		case 't':
+          break;
+    		case 'A':
+    		case 'a':
+          break;
+    		case 'Z':
+    		case 'z':
+    		}
+    	}
     }
-  	pp.reset();
-  
-  	while (!pp.isEnd()) {
-  		pp.nextCommand();
-  		switch (pp.command) {
-  		case 'M':
-  		case 'm':
-  			var p = pp.getAsCurrentPoint(pp.isRelativeCommand());
-  			var cmd = new Object();
-        cmd.command = pp.command;
-        cmd.parameters = new Array();
-        cmd.parameters.push(p);
-        cmdList.push(cmd);
-  			pp.start = pp.current;
-  			while (!pp.isCommandOrEnd()) {
-  				var p = pp.getAsCurrentPoint(pp.isRelativeCommand());
-  				var cmd = new Object();
-          cmd.command = "M";
-          cmd.parameters = new Array();
-          cmd.parameters.push(p);
-          cmdList.push(cmd);
-  			}
-  			break;
-  		case 'L':
-  		case 'l':
-  			while (!pp.isCommandOrEnd()) {
-  				var cmd = new Object();
-          cmd.current = pp.current;
-  				var p = pp.getAsCurrentPoint(pp.isRelativeCommand());
-          cmd.command = pp.command;
-          cmd.parameters = new Array();
-          cmd.parameters.push(p);
-          cmd.endPt = p;
-          cmdList.push(cmd);
-  			}
-  			break;
-  		case 'H':
-  		case 'h':
-  			while (!pp.isCommandOrEnd()) {
-  				var cmd = new Object();
-          cmd.current = pp.current;
-  			  var coord = pp.getScalar();
-  			  var t =   pp.isRelativeCommand();
-  				var newP = new pp.Point((pp.isRelativeCommand() ? pp.current.x : 0) + coord, pp.current.y);
-  				pp.current = newP;
-          cmd.command = pp.command;
-          cmd.parameters = new Array();
-          cmd.parameters.push((pp.isRelativeCommand() ? pp.current.x : 0) + coord);
-          cmd.endPt = newP;
-          cmdList.push(cmd);
-  			}
-  			break;
-  		case 'V':
-  		case 'v':
-  			while (!pp.isCommandOrEnd()) {
-  				var cmd = new Object();
-          cmd.current = pp.current;
-  			  var coord = pp.getScalar();
-  				var newP = new pp.Point(pp.current.x, (pp.isRelativeCommand() ? pp.current.y : 0) + coord);
-  				pp.current = newP;
-          cmd.command = pp.command;
-          cmd.parameters = new Array();
-          cmd.parameters.push((pp.isRelativeCommand() ? pp.current.x : 0) + coord);
-          cmd.endPt = newP;
-          cmdList.push(cmd);
-  			}
-  			break;
-  		case 'C':
-  		case 'c':
-        while (!pp.isCommandOrEnd()) {
-  				var cmd = new Object();
-          cmd.current = pp.current;
-  				var cntrl1 = pp.getAsControlPoint(pp.isRelativeCommand());
-  				var cntrl2 = pp.getAsControlPoint(pp.isRelativeCommand());
-  				var cp = pp.getAsCurrentPoint(pp.isRelativeCommand());
-          cmd.command = pp.command;
-          cmd.parameters = new Array();
-          cmd.parameters.push(cntrl1);
-          cmd.parameters.push(cntrl2);
-          cmd.parameters.push(cp);
-          cmdList.push(cmd);
-  			}
-        break;
-  		case 'S':
-  		case 's':
-  			/*
-        while (!pp.isCommandOrEnd()) {
-  				var curr = pp.current;
-  				var p1 = pp.getReflectedControlPoint();
-  				var cntrl = pp.getAsControlPoint(pp.isRelativeCommand());
-  				var cp = pp.getAsCurrentPoint(pp.isRelativeCommand());
-  				pp.addMarker(cp, cntrl, p1);
-  				bb.addBezierCurve(curr.x, curr.y, p1.x, p1.y, cntrl.x, cntrl.y, cp.x, cp.y);
-  			}
-  			*/
-        break;
-  		case 'Q':
-  		case 'q':
-        while (!pp.isCommandOrEnd()) {
-  				var cmd = new Object();
-          cmd.current = pp.current;
-  				var cntrl = pp.getAsControlPoint(pp.isRelativeCommand());
-  				var cp = pp.getAsCurrentPoint(pp.isRelativeCommand());
-          cmd.command = pp.command;
-          cmd.parameters = new Array();
-          cmd.parameters.push(cntrl);
-          cmd.parameters.push(cp);
-          cmd.endPt = cp;
-          cmdList.push(cmd);
-  			}
-        break;
-  		case 'T':
-  		case 't':
-        break;
-  		case 'A':
-  		case 'a':
-        break;
-  		case 'Z':
-  		case 'z':
-  		}
-  	}
-  }
-  // add a null command "" with the current point
-	var cmd = new Object();
-  cmd.current = pp.current;
-  cmd.command = "";
-  cmdList.push(cmd);
- 
-  return cmdList;
+    // add a null command "" with the current point
+  	var cmd = new Object();
+    cmd.current = pp.current;
+    cmd.command = "";
+    cmdList.push(cmd);
+   
+    return cmdList;
   }
 }
