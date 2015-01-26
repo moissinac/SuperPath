85f9de9b069ccd531afc47c1bee22f2f490472c9
Jean-Claude Moissinac <jcmoissinac@gmail.com>
2015-01-26 16:29:45 +0100

    added some tests mainly to check some functionalities in regard of chunk processing the actual code and tests checks only M, l and L commands

--- Tests/tests.js
+++ Tests/tests.js
@@ -1,3 +1,45 @@
+QUnit.test( "Test building command list from data", function( assert ) {
+  var desc = "l0,0l-50,80l50,80 -50,80", 
+      startingPt,
+      data,
+      cmdList;
+  startingPt = {};
+  startingPt.x = 330;
+  startingPt.y = 150;
+  data = "M"+startingPt.x+","+startingPt.y+desc;
+  cmdList = superpath.svg_parse_path(data);
+  assert.equal( cmdList.toString(), "M330,150l0,0l-50,80l50,80l-50,80", "Passed!" );
+});
+QUnit.test( "Test building command list from data and transform to relative", function( assert ) {
+  var desc = "l0,0l-50,80l50,80 -50,80", 
+      startingPt,
+      data,
+      cmdList,
+      relCmdList;
+  startingPt = {};
+  startingPt.x = 330;
+  startingPt.y = 150;
+  data = "M"+startingPt.x+","+startingPt.y+desc;
+  cmdList = superpath.svg_parse_path(data);
+  relCmdList = superpath.fullrelativePathCmdList(cmdList);
+  assert.equal( relCmdList.toString(), "M330,150l0,0l-50,80l50,80l-50,80", "Passed!" );
+});
+QUnit.test( "Test building command list from data and transform to relative and reverse", function( assert ) {
+  var desc = "l0,0l-50,80l50,80 -50,80", 
+      startingPt,
+      data,
+      cmdList,
+      relCmdList,
+      revCmdList;
+  startingPt = {};
+  startingPt.x = 330;
+  startingPt.y = 150;
+  data = "M"+startingPt.x+","+startingPt.y+desc;
+  cmdList = superpath.svg_parse_path(data);
+  relCmdList = superpath.fullrelativePathCmdList(cmdList);
+  revCmdList = relCmdList.reverse();
+  assert.equal( revCmdList.toString(), "l50,-80l-50,-80l50,-80l0,0M-330,-150", "Passed!" );
+});
 QUnit.test( "Test elementary path reversion", function( assert ) {
   //var sp = new superpath();
   var newdata = superpath.reversePathData("M 90,190L220,90");
@@ -19,4 +61,43 @@ QUnit.test( "Test path transformation to reversed fully relative path", function
     var data = "M90,190 220,90 330,150 330,150 280,230 330,310 280,400 120,375 z";
     rData = superpath.reversePathData2(data);
     assert.equal( rData, "M120,375l160,25l50,-90l-50,-80l50,-80l0,0l-110,-60l-130,100z", "Passed!");
+});
+QUnit.test( "Test parsing path data with superpath definition extension", function( assert ) {
+    var data = "M 90,190 220,90 330,150 (p1|L330,150 280,230 L330,310 280,400)L120,375 z";
+    var cmdList = superpath.svg_parse_path(data);
+    var strCmdList = cmdList.toString(cmdList);
+    assert.equal( strCmdList, "M90,190L220,90L330,150(p1|L330 150 280 230 L 330 310 280 400 )L120,375z", "Passed!");
+});
+QUnit.test( "Test finding current starting point for a subpath definition with an absolute path", function( assert ) {
+    var data = "M 90,190 220,90 330,150 (p1|L280,230 L330,310 280,400)L120,375 z";
+    var cmdList = superpath.svg_parse_path(data);
+    cmdList.processCurrentPoints();
+    var pt = cmdList.getSubpathStartingPoint("p1");
+    assert.equal( pt.x+" "+pt.y, "330 150", "Passed!");
+});
+QUnit.test( "Test finding current starting point for a subpath definition with a relative path, with absolute positions preprocessing", function( assert ) {
+    var data = "M90,190l130,-100 110,60 (p1|l-50,80l50,80 -50,90)l-160,-25 z";
+    var cmdList = superpath.svg_parse_path(data);
+    cmdList.processCurrentPoints();
+    var pt = cmdList.getSubpathStartingPoint("p1");
+    assert.equal( pt.x+" "+pt.y, "330 150", "Passed!");
+});
+QUnit.test( "Test finding current starting point for a subpath definition with a mixed path, with absolute positions preprocessing", function( assert ) {
+    var data = "M90,190l130,-100 110,60 (p1|L280,230 L330,310 280,400)l-160,-25 z";
+    var cmdList = superpath.svg_parse_path(data);
+    cmdList.processCurrentPoints();
+    var pt = cmdList.getSubpathStartingPoint("p1");
+    assert.equal( pt.x+" "+pt.y, "330 150", "Passed!");
+});
+QUnit.test( "Test finding current starting point for a subpath definition with a relative path, without absolute positions preprocessing", function( assert ) {
+    var data = "M90,190l130,-100 110,60 (p1|l-50,80l50,80 -50,90)l-160,-25 z";
+    var cmdList = superpath.svg_parse_path(data);
+    var pt = cmdList.getSubpathStartingPoint("p1");
+    assert.equal( pt.x+" "+pt.y, "330 150", "Passed!");
+});
+QUnit.test( "Test finding current starting point for a subpath definition with a mixed path, without absolute positions preprocessing", function( assert ) {
+    var data = "M90,190l130,-100 110,60 (p1|L280,230 L330,310 280,400)l-160,-25 z";
+    var cmdList = superpath.svg_parse_path(data);
+    var pt = cmdList.getSubpathStartingPoint("p1");
+    assert.equal( pt.x+" "+pt.y, "330 150", "Passed!");
 });
