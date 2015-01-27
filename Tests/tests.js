0311cf14a961ef5cac1bf29b2c18dc612018f875
Jean-Claude Moissinac <jcmoissinac@gmail.com>
2015-01-27 13:28:02 +0100

    remove test using reversePathData2, which has been deleted

--- Tests/tests.js
+++ Tests/tests.js
@@ -64,13 +64,7 @@ QUnit.test( "Test path transformation to fully relative path", function( assert 
     var data = "M90,190 220,90 330,150 330,150 280,230 330,310 280,400 120,375 z";
     var cmdList = superpath.svg_parse_path(data);
     cmdList = superpath.fullrelativePathCmdList(cmdList);
-    rData = superpath.svgSerializeCmdList(cmdList);
-    assert.equal( rData, "M90,190l130,-100l110,60l0,0l-50,80l50,80l-50,90l-160,-25z", "Passed!");
-});
-QUnit.test( "Test path transformation to reversed fully relative path", function( assert ) {
-    var data = "M90,190 220,90 330,150 330,150 280,230 330,310 280,400 120,375 z";
-    rData = superpath.reversePathData2(data);
-    assert.equal( rData, "M120,375l160,25l50,-90l-50,-80l50,-80l0,0l-110,-60l-130,100z", "Passed!");
+    assert.equal( cmdList.toString(), "M90,190l130,-100l110,60l0,0l-50,80l50,80l-50,90l-160,-25z", "Passed!");
 });
 QUnit.test( "Test parsing path data with superpath definition extension", function( assert ) {
     var data = "M 90,190 220,90 330,150 (p1|L330,150 280,230 L330,310 280,400)L120,375 z";
